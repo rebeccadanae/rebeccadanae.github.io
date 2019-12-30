4af9a3f191cda3841337bc4ff2f1403033902cd2
rebeccadanae <rebeccadportman@gmail.com>
2019-12-30 14:22:40 -0500

    set up clustered bar chart

--- app.js
+++ app.js
@@ -24,19 +24,25 @@
 // Parse the date / time
 var	parseDate = d3.time.format("%Y-%m").parse;
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+var x0 = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
-var y = d3.scale.linear().range([height, 0]);
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
+    .scale(x0)
+    .tickSize(0)
+    .orient("bottom");
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left")
-    .ticks(5);
+    .orient("left");
+
+var color = d3.scale.ordinal()
+    .range(["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0"]);
 
 var svg = d3.select(".graph-left").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -45,45 +51,61 @@ var svg = d3.select(".graph-left").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("bar-data.csv", function(error, data) {
+          d3.json("data.json", function(error, data) {
 
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.value = +d.value;
-    });
+            var categoriesNames = data.map(function(d) { return d.categorie; });
+            var rateNames = data[0].values.map(function(d) { return d.rate; });
 
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+            x0.domain(categoriesNames);
+            x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
+            y.domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", "-3em")
-      .style("text-anchor", "end")
-      .text("Percent decrease in eligibility");
+            svg.append("g")
+                .attr("class", "y axis")
+                .style('opacity','0')
+                .call(yAxis)
+            .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .style('font-weight','bold')
+                .text("Value");
 
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.date); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
+            svg.select('.y').transition().duration(500).delay(1300).style('opacity','1');
 
+            var slice = svg.selectAll(".slice")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "g")
+                .attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+            slice.selectAll("rect")
+                .data(function(d) { return d.values; })
+            .enter().append("rect")
+                .attr("width", x1.rangeBand())
+                .attr("x", function(d) { return x1(d.rate); })
+                .style("fill", function(d) { return color(d.rate) })
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return height - y(0); })
+                .on("mouseover", function(d) {
+                    d3.select(this).style("fill", d3.rgb(color(d.rate)).darker(2));
+                })
+                .on("mouseout", function(d) {
+                    d3.select(this).style("fill", color(d.rate));
+                });
+
+            slice.selectAll("rect")
+                .transition()
+                .delay(function (d) {return Math.random()*1000;})
+                .duration(1000)
+                .attr("y", function(d) { return y(d.value); })
+                .attr("height", function(d) { return height - y(d.value); });
 });
   }
 
